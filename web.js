0c61fc14b4777bac0fc1257f72bf7ed96d57cd7e
samyamsharma7 <157783528+samyamsharma7@users.noreply.github.com>
2024-01-27 20:30:49 +0545

    Add files via upload

---
+++ web.js
@@ -0,0 +1,26 @@
+function home()
+{
+    window.location.href = './index.html';
+}
+
+function about()
+{
+    var about = document.getElementById('about_id');
+    about.scrollIntoView({behavior: "smooth"});
+}
+
+function contact()
+{
+    var contact = document.getElementById('contact_id');
+    contact.scrollIntoView({behavior: "smooth"});
+}
+
+function fb()
+{
+    window.location.href = 'https://www.facebook.com/samyam.sharma777/';
+}
+
+function insta()
+{
+    window.location.href = 'https://www.instagram.com/samyamsharma007/';
+}
